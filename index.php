21de43e1dca34e03b567d1e20f7aa5bdd9298ce7
ram1071999 <ramvashishtha2017@gmail.com>
2021-11-11 01:06:36 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,393 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Blog</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="    sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+	<!---------------bootstrap cdn----------------------------->
+
+	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" id="ff">
+	<div class="container">
+  <a class="navbar-brand" href="#"><i class="far fa-envelope"></i>&nbsp;&nbsp;ramvashishtha2017@gmail.com</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#"><i class="fab fa-facebook"></i> <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#"><i class="fab fa-instagram"></i></a>
+      <a class="nav-item nav-link" href="#"><i class="fab fa-twitter"></i></a>
+      <a class="nav-item nav-link " href="#"><i class="fab fa-linkedin-in"></i></a>
+    </div>
+  </div>
+</div>
+</nav>
+<nav class="fixed-top w-100 bg-white mt-5">
+	<div id="logo">
+		<img src="http://localhost/first/images/mainlogo.png">
+		
+	</div>
+</nav>
+
+ <div  id="ws" class="fixed-bottom"> <a href="https://wa.link/9e09i1" class="ws mb-auto mt-5" >
+<i class="fab fa-whatsapp  display-4 text-success mt-5 ws" margin-left="10%"></i>
+</a>
+</div>
+
+
+<!-------------------navigation closed----------------------------->
+<!-------------------card-started------------------->
+  
+<div class="container-fluid py-2" id="mainbox">
+  <h3 class="py-5 pt-5 text-center"> Webpage </h3>
+  
+<hr>
+
+
+  <!--------------------1 row----------->
+	<div class="row pt-3">
+
+
+<!-----------------------1 card------------------->
+<div class="col-md-3 " id="">
+ <!-- Social buttons -->
+ 
+  <div class="card " >
+   <div class="card-share">
+               <a class="btn-floating btn-action share-toggle indigo ml-auto mr-4 float-right"><i class="fas fa-share-alt"></i></a>
+  </div>
+ <!-- Card content -->
+    <div class="card-body"><!-- Title -->
+      <a href="https://www.amazon.in/ref=nav_logo"><h4 class="card-title">Informative</h4></a>
+           <hr>
+    <!-- Text -->
+         <p class="card-text">The definition of informative is something that contains useful.  
+                   It's really informative and includes lots of information about gestational diabetes, including a great sample diet plan, nutrition tips. </p>
+       <button class="btn btn-indigo btn-rounded btn-md">Read more</button>
+
+    </div>
+  </div>
+
+</div>
+<!-------------1 card------------------->
+
+
+<div class="col-md-3 ">
+
+	<div class="card">
+ <!-- Social buttons -->
+    <div class="card-share">
+               <a class="btn-floating btn-action share-toggle indigo ml-auto mr-4 float-right"><i class="fas fa-share-alt"></i></a>
+   </div>
+ <!-- Card content -->
+     <div class="card-body"><!-- Title -->
+       <a href=""> <h4 class="card-title">Business</h4></a>
+           <hr>
+    <!-- Text -->
+         <p class="card-text">Its defined as an organization or enterprising entity engaged in commercial, industrial, or professional activities. Businesses can be for-profit entities or non-profit organizations. 
+         </p>
+      <button class="btn btn-indigo btn-rounded btn-md">Read more</button>
+
+    </div>
+</div>
+</div>
+<!------------------2-card------------------>
+<div class=" col-md-3">
+ <div class="card py-2 ">
+ <!-- Social buttons -->
+   <div class="card-share">
+               <a class="btn-floating btn-action share-toggle indigo ml-auto mr-4 float-right"><i class="fas fa-share-alt"></i></a>
+   </div>
+ <!-- Card content -->
+   <div class="card-body"><!-- Title -->
+    <a href="#"><h4 class="card-title">Editable</h4></a> 
+           <hr>
+    <!-- Text -->
+         <p class="card-text"> Any element can be editable. By using some JavaScript event handlers, you can transform your web page into a full and fast rich text editor</p>
+      <button class="btn btn-indigo btn-rounded btn-md">Read more</button>
+
+   </div>
+</div>
+
+</div>
+<div class="col-md-3">
+ <!-- Social buttons -->
+ <div class="card">
+     <div class="card-share">
+               <a class="btn-floating btn-action share-toggle indigo ml-auto mr-4 float-right"><i class="fas fa-share-alt"></i></a>
+    </div>
+ <!-- Card content -->
+    <div class="card-body"><!-- Title -->
+    <a href=""> <h4 class="card-title py-1">E-commerce</h4></a>
+           <hr>
+    <!-- Text -->
+         <p class="card-text">E-commerce is the buying and selling of goods and services, or the transmitting of funds or data. These business transactions occur either as B2B , B2C, C2C or C2B.</p>
+      <button class="btn btn-indigo btn-rounded btn-md">Read more</button>
+
+     </div>
+  </div>
+
+<!-- Card  closed-->
+	
+</div>
+</div>
+<h3 class=" pt-3 text-center" >Programming</h3>
+<!-------------------2 row--------------->
+<div class="row py-3">
+
+
+
+<!-----------------------1 card------------------->
+<!--  1 Card  -->
+
+<div class="col-md-4">
+ <!-- Social buttons -->
+ <div class="card">
+  
+ <!-- Social buttons -->
+    <div class="card-share">
+               <a class="btn-floating btn-action share-toggle indigo ml-auto mr-4 float-right"><i class="fas fa-share-alt"></i></a>
+   </div>
+ <!-- Card content -->
+     <div class="card-body"><!-- Title -->
+       <a href="#"><h4 class="card-title">Php</h4></a>
+           <hr>
+    <!-- Text -->
+         <p class="card-text">PHP is a recursive acronym for "PHP: Hypertext Preprocessor".PHP is a server side scripting language that is embedded in HTML.
+        It is used to manage dynamic content, databases, 
+       session tracking, even build entire e-commerce sites.</p>
+      <button class="btn btn-indigo btn-rounded btn-md">Read more</button>
+
+    </div>
+  
+</div>
+
+</div>
+
+<!-------------1 card------------------->
+
+
+<div class="col-md-4">
+	 <div class="card">
+ <!-- Social buttons -->
+  <div class="card-share">
+               <a class="btn-floating btn-action share-toggle indigo ml-auto mr-4 float-right"><i class="fas fa-share-alt"></i></a>
+ </div>
+ <!-- Card content -->
+   <div class="card-body"><!-- Title -->
+    <a href=""> <h4 class="card-title">Html</h4></a>
+           <hr>
+    <!-- Text -->
+         <p class="card-text">HyperText Markup Language (HTML) is the set of markup symbols or
+ codes inserted into a file intended for display on the Internet.
+ The markup tells web browsers how to display a web page's words and images.</p>
+      <button class="btn btn-indigo btn-rounded btn-md">Read more</button>
+
+  </div>
+</div>
+</div>
+
+<div class=" col-md-4">
+ <div class="card py-2" >
+  <div class="card-share">
+               <a class="btn-floating btn-action share-toggle indigo ml-auto mr-4 float-right"><i class="fas fa-share-alt"></i></a>
+ </div>
+ <!-- Card content -->
+   <div class="card-body"><!-- Title -->
+     <a href="#"><h4 class="card-title">CSS</h4></a>
+           <hr>
+    <!-- Text -->
+         <p class="card-text">CSS stands for Cascading Style Sheets
+CSS describes how HTML elements are to be displayed on screen, paper, or in other media
+CSS saves a lot of work.External stylesheets are stored in CSS files
+       </p>
+      <button class="btn btn-indigo btn-rounded btn-md">Read more</button>
+
+  </div>
+  </div>
+</div>
+
+<!------------------2-card------------------>
+
+</div>
+<!------------row closed------------>
+<!----------3 row-------------->
+<div class="row py-1">
+  <div class="col-md-4 ">
+ <!-- Social buttons -->
+ <div class="card " >
+  <div class="card-share">
+               <a class="btn-floating btn-action share-toggle indigo ml-auto mr-4 float-right"><i class="fas fa-share-alt"></i></a>
+ </div>
+ <!-- Card content -->
+   <div class="card-body"><!-- Title -->
+   <a href=""><h4 class="card-title">Javascript</h4></a> 
+           <hr>
+    <!-- Text -->
+         <p class="card-text py-1">JavaScript is a dynamic computer programming language. whose implementations allow client-side script to interact with the user and make dynamic pages. </p>
+      <button class="btn btn-indigo btn-rounded btn-md">Read more</button>
+
+  </div>
+  </div>
+</div>
+  <div class="col-md-4 ">
+ <!-- Social buttons -->
+ <div class="card " >
+  <div class="card-share">
+               <a class="btn-floating btn-action share-toggle indigo ml-auto mr-4 float-right"><i class="fas fa-share-alt"></i></a>
+ </div>
+ <!-- Card content -->
+   <div class="card-body"><!-- Title -->
+    <a href="#"> <h4 class="card-title">jQuery</h4></a>
+           <hr>
+    <!-- Text -->
+         <p class="card-text">jQuery is a lightweight, "write less, do more", JavaScript library.
+
+The purpose of jQuery is to make it much easier to use JavaScript on your website.
+
+jQuery takes a lot of common tasks that require many lines of JavaScript code to accomplish. </p>
+      <button class="btn btn-indigo btn-rounded btn-md">Read more</button>
+
+  </div>
+  </div>
+</div>
+<div class="col-md-4 ">
+ <!-- Social buttons -->
+ <div class="card">
+ <!-- Social buttons -->
+  <div class="card-share">
+               <a class="btn-floating btn-action share-toggle indigo ml-auto mr-4 float-right"><i class="fas fa-share-alt"></i></a>
+ </div>
+ <!-- Card content -->
+   <div class="card-body"><!-- Title -->
+     <a href="#"><h4 class="card-title">SQL</h4></a>
+           <hr>
+    <!-- Text -->
+         <p class="card-text">SQL stands for Structured Query Language
+SQL lets you access and manipulate databases
+SQL became a standard of the American National Standards Institute (ANSI) in 1986, and of the International Organization for Standardization (ISO) in 1987 .
+ </p>
+      <button class="btn btn-indigo btn-rounded btn-md">Read more</button>
+
+  </div>
+</div>
+</div>
+<!-------------1 card------------------->
+
+
+<!------------------2-card------------------>
+
+<!-- Card  closed-->
+  
+</div>
+
+</div>
+	
+
+
+              <!--------------------- Footer --------------------------------------->
+<footer class="page-footer font-small blue pt-4">
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-6 mt-md-0 mt-3 justify-content-center" id="add_f">
+
+        <!-- Content -->
+        <div class="address">
+      <h3>Namrata Universal</h3>
+        <p><i class="fas fa-map-marker-alt"></i> 3C, A1 Sector 10 Noida</p>
+        <p> Uttar Pradesh, India</p>
+
+          <p><i class="fas fa-phone-volume "></i>&nbsp;&nbsp;01204350224</p>
+		
+       </div>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3" id="col2">
+
+        <!-- Links -->
+        <h5 class="text-uppercase">Useful Links</h5>
+
+
+
+
+
+
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Home</a>
+          </li>
+          <li>
+            <a href="#!">About us</a>
+          </li>
+          <li>
+            <a href="#!">Services</a>
+          </li>
+          <li>
+            <a href="#!">Terms of service</a>
+          </li>
+
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3" id="icon">
+
+        <!-- Links -->
+        <h5 class="text-uppercase text-white">Social contact</h5>
+
+        <ul class="list-unstyled pl-4">
+          <li>
+            <a href="#"><i class="fab fa-facebook"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+  <hr>
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2021 Copyright:
+    <a href="#"> MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+</body>
+</html>
